2754fee05d312709c06591e44bedcde1aa530825
Balla Kavya <ballakavya594@gmail.com>
2025-01-27 14:53:09 +0530

    771. Jewels and stones

---
+++ 771.jewels and stones.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count=0
+        for i in stones:
+            if i in jewels:
+                count=count+1
+        
+        return count
+
+        
