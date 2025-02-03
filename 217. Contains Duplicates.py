c86970042ae22cd5a887219f5f2d019176c95087
Balla Kavya <ballakavya594@gmail.com>
2025-02-03 20:40:34 +0530

    217. Contains Duplicates

---
+++ 217. Contains Duplicates.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        set1=set()
+        for i in nums:
+            if i in set1:
+                return True
+            set1.add(i)
+        return False
