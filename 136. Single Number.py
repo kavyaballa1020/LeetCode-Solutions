d01c8b4144997d0d8da50edbab044c5cedc72e0a
Balla Kavya <ballakavya594@gmail.com>
2025-02-07 11:19:46 +0530

    136. Single Number.py

---
+++ 136. Single Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        freq=Counter(nums)
+        for i in nums:
+            if freq[i]==1:
+                return i
